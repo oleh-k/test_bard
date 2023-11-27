58df5470e0395269a9d97113492e22270b7b50e9
oleh_ok <oleh.k@hotmail.com>
2023-11-27 19:57:24 +0200

    create route and controller

---
+++ app/Http/Controllers/BardController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BardController extends Controller
+{
+    public function search(Request $request)
+    {
+        return 'bard search';
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BardController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/bard', [BardController::class, 'search']);
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
