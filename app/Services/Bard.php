a78d79eed7ce04894e757610c7df88bfe1662efb
oleh_ok <oleh.k@hotmail.com>
2023-11-27 20:07:40 +0200

    add Validator

--- app/Services/Bard.php
+++ app/Services/Bard.php
@@ -10,11 +10,23 @@ class Bard
     public static function search($request): array
     {
 
+        $validator = Validator::make($request, [
+            'question' => 'required|min:2|max:250'
+        ]);
 
-        $response = [
-            "success" => true,
-            "message" => $request['question']
-        ];
-        return $response;
+        if ($validator->fails()) {
+            $response = [
+                "success" => false,
+                "message" => $validator->errors()
+            ];
+            return $response;
+        } else {
+
+            $response = [
+                "success" => true,
+                "message" => $request['question']
+            ];
+            return $response;
+        }
     }
 }
